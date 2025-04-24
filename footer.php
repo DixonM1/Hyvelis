8e7350112a0526c450815c2ff009a20c2509c1c1
DixonM1 <xitrexdank@gmail.com>
2025-04-24 06:42:17 -0600

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,56 @@
+<footer>
+  <div class="site-wrapper">
+    <div class="top-footer">
+      <div class="site-row">
+        <div class="site-col col-1">
+          <div class="ftr-logo">
+            <a href="index.php">
+              <img src="images/hyvelis-logo.svg" alt="Hyvelis Logo">
+            </a>
+          </div>
+        </div>
+        <div class="site-col col-5">
+          <div class="ftr-subscribe">
+            <h6>Suscríbete a nuestro boletín</h6>
+            <form action="">
+              <input type="text" class="form-control" placeholder="Ingresa tu correo electrónico...">
+              <input type="submit" value="Enviar" class="site-btn">
+            </form>
+          </div>
+        </div>
+        <div class="site-col col-2">
+          <div class="ftr-nav">
+            <h6>Enlaces Rápidos</h6>
+            <ul>
+              <li><a href="index.php">Inicio</a></li>
+              <li><a href="#about-us">Quiénes Somos</a></li>
+              <li><a href="servicios.php">Servicios</a></li>
+              <li><a href="agendar-cita.php">Agendar Cita</a></li>
+              <li><a href="unirse-como-personal.php">Unirse como Personal</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="site-col col-3">
+          <div class="ftr-nav">
+            <h6>Ubicación</h6>
+            <p>Tegucigalpa, Honduras</p>
+            <div class="ftr-contact">
+              <p><span class="icon"><img src="images/footer-mobile.svg" alt=""></span> +504 1234-5678</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="bottom-footer">
+      <div class="ftr-social d-flex">
+        <p>Síguenos</p>
+        <a href="#"><img src="images/fb-icon.svg" alt="Facebook"></a>
+        <a href="#"><img src="images/insta-icon.svg" alt="Instagram"></a>
+      </div>
+      <p class="site-copyright">© 2025 Hyvelis. Todos los derechos reservados. Diseñado por <a href="#" target="_blank">Hyvelis Team</a></p>
+    </div>
+  </div>
+</footer>
+<script src="js/script.js"></script>
+</body>
+</html>
